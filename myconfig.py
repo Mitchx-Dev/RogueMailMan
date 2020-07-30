aebc2a4e786e971929cfca283edb3b887a88765a
MitchX <mehricke99@gmail.com>
2020-07-29 22:55:32 -0500

    Add files via upload

---
+++ myconfig.py
@@ -0,0 +1,36 @@
+# options
+show_chrome_window = True
+checkout_as_guest_paypal = False
+
+# General Info
+artist = "muzikandfriendz"
+album = "Last Call"
+keyword = "exclusive"
+discord_webhook = "url"
+
+# PayPal Info
+paypal_email = "admin@mitchx.dev"
+paypal_password = "78OHx1IO0JdY"
+
+# Shipping Info
+name = "Firstname Lastname"
+country_shipping = "Colombia"
+address = "1234 glizzy st."
+postal_code = "77458"
+shipping_city = "Chicago"
+state_province_region = "California"
+
+# Billing Info
+country_billing = "United States"
+card_no = "4346112541236487"
+exp_date = "2322" #in MMYY Format
+cvc_no = "232"
+billing_first_name = "Louis"
+billing_last_name = "Jonson"
+phone_no = "6549975286"
+billing_street_address = "1234 Sesame St."
+billing_street_address_2 = ""
+billing_city = "Denver"
+billing_postal_code = "99182"
+billing_state_province_district = "Rhode Island"
+billing_email = 'Cronk@gmail.com'
